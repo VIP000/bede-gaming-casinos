893de1419338f3075fd2ec1deeb2fabdb22b49a1
Alan Wynn <alan.wynn@me.com>
2016-05-10 21:15:29 +0100

    add some npm deps and update gitignore
    Signed-off-by: Alan Wynn <alan.wynn@me.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var elixir = require('laravel-elixir');
+require('laravel-elixir-vueify');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +13,29 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix
+        // Scss Styles
+        .sass('app.scss')
+
+        // Scripts
+        .browserify('app.js')
+
+        // Bootstrap fonts
+        .copy('node_modules/bootstrap-sass/assets/fonts', elixir.config.publicPath + '/fonts')
+
+        // Font Awesome fonts
+        .copy('node_modules/font-awesome/fonts', elixir.config.publicPath + '/fonts')
+
+        // Copy any fonts
+        .copy(elixir.config.publicPath + '/fonts', elixir.config.publicPath + '/build/fonts')
+
+        // Copy any Images
+        .copy(elixir.config.assetsPath + '/images', elixir.config.publicPath + '/images')
+        .copy(elixir.config.assetsPath + '/images', elixir.config.publicPath + '/build/images')
+
+        // Version the files for cache busting
+        .version([
+            'js/app.js',
+            'css/app.css'
+        ]);
 });
